68fd1daf27e047808b67764426e11904ac2c9951
Trevor Shaddox <tshaddox@ucla.edu>
2011-07-07 20:45:29 +0000

    Saving recent changes
    git-svn-id: http://bsccs.googlecode.com/svn/branches/hierarchy@54 f7c91f03-731f-d806-b965-e12744d8cf89

---
+++ C/codebase/CCD/HierarchyReader.h
@@ -0,0 +1,41 @@
+/*
+ * HierarchyReader.h
+ *
+ *  Created on: Jun 7, 2011
+ *      Author: tshaddox
+ */
+
+#ifndef HIERARCHYREADER_H_
+#define HIERARCHYREADER_H_
+
+#include <iostream>
+#include <fstream>
+
+#include <vector>
+#include <map>
+
+using namespace std;
+
+#include "CyclicCoordinateDescent.h"
+
+//#define USE_DRUG_STRING
+
+#ifdef USE_DRUG_STRING
+	typedef string DrugIdType; // TODO String do not get sorted in numerical order
+#else
+	typedef int DrugIdType;
+#endif
+
+class HierarchyReader: public CompressedIndicatorMatrix {
+public:
+	HierarchyReader();
+
+	HierarchyReader(const char* fileName, map<int, int> &drugIdToIndex, CyclicCoordinateDescent **ccd);
+
+
+private:
+
+
+};
+
+#endif /* HIERARCHYREADER_H_ */
